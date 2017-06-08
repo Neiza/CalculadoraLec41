75b9ee30a685be18f02937afa13d6f83eedc3764
Neiza Nunez <neizapf@gmail.com>
2017-06-08 16:53:58 -0500

    third

--- aseets/js/app.js
+++ aseets/js/app.js
@@ -1,19 +1,19 @@
-"use strict"
-$(_ =>{
+$( _ =>{
+
   let screenVal = $("input:text");
 
-$("input:button").on("click", _ =>{
-  const eachNumber  = $("input:button").val();
-  let currentScreen = screenVal.val();
-  currentScreen += eachNumber
-  screenVal.val(currentScreen);
-  if ($(this).value == "C"){
-    screenVal.val("");
-  }
-});
+  $("input:button").on("click", _ =>{
+    let eachNumber  = $("input:button").val();
+    let currentScreen = screenVal.val();
+    currentScreen += eachNumber;
+    screenVal.val(currentScreen);
+    if ($(this).val() == "C"){
+      screenVal.val("");
+    }
+  });
 
-$("button").click((event) =>{
-  event.preventDefault();
-  screenVal.val(eval(screenVal.val()));
-});
+  $("button").click((event) =>{
+    event.preventDefault();
+    screenVal.val(eval(screenVal.val()));
+  });
 });
