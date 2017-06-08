fee13fd29e6f73f3d6ca7596d8a70c77c38c6178
Neiza Nunez <neizapf@gmail.com>
2017-06-08 15:55:58 -0500

    First

---
+++ aseets/js/app.js
@@ -0,0 +1,19 @@
+"use strict"
+$(_ =>{
+  const screenVal = $("input:text");
+
+$("input:button").on("click", _ =>{
+  const eachNumber  = $("input:button").val();
+  let currentScreen = screenVal.val();
+  currentScreen += eachNumber
+  screenVal.val(currentScreen);
+  if ($(this).value == "C"){
+    screenVal.val("");
+  }
+});
+
+$("button").click((event) =>{
+  event.preventDefault();
+  screenVal.val(eval(screenVal.val()));
+});
+});
